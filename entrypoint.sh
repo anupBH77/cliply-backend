3ef0668b425f0600e9e2aa5a6856166df1df4cfe
anupBH77 <anupbhaumik44@gmail.com>
2026-06-19 23:51:11 +0530

    Replace gunicorn with uvicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ alembic upgrade head
 
 echo "Starting application..."
 
-exec gunicorn app.main:app \
-    -k uvicorn.workers.UvicornWorker \
-    -w 2 \
-    -b 0.0.0.0:${PORT:-8000}
+exec uvicorn app.main:app \
+    --host 0.0.0.0 \
+    --port "${PORT:-8000}"
